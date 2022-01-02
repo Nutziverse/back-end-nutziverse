d30ea085f5a7a5f2c079e33a4bb2e0ea0116a09c
lisapriliani <lisaapriliani4@gmail.com>
2022-01-02 20:35:21 +0700

    add dataToken

--- controllers/profile.controller.js
+++ controllers/profile.controller.js
@@ -1,9 +1,12 @@
+const { dataToken } = require("../helpers");
 const ProfileModel = require("../models/profile.model");
 
 class profileController {
   static async getProfile(req, res) {
+    const { data } = dataToken(req, res);
     try {
-      const profile = await ProfileModel.find();
+      const UID = data._id;
+      const profile = await ProfileModel.findOne({ _id: UID }, { _id, nama, email, no_hp, umur, jeniskelamin, berat, tinggi });
       res.send(profile);
     } catch (error) {
       res.status(500);
