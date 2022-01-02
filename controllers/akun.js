a27040737d304228e9d54c39bfc34cba97aee8d3
Jessica Wong <wongjessica124@gmail.com>
2022-01-02 15:41:15 +0700

    import UserModel and import token middleware to router

--- controllers/akun.js
+++ controllers/akun.js
@@ -1,32 +1,38 @@
 
 // import user model
+const { dataToken } = require("../helpers")
+const UserModel = require("../models/users.model")
 
-// get user id
 const getAkun = async (req, res) => {
+  const {data} = dataToken(req, res)
+  
   try {
     // get by user id
-    const akun = null
-
+    const UID = data._id
+    const akun = await UserModel.findOne({_id: UID}, {_id: 0, email: 1, no_hp: 1, password: 1})
+    
     res.send(akun)
     
   } catch (error) {
-    res.status(500).send({error: error})
+    res.status(500).send({error: error.message})
   }
 }
 
 const editAkun = async (req, res) => {
-  const data = req.body
+  const {data} = dataToken(req, res)
+  const updateData = req.body
+  
   try {
     // edit akun user by id
-    // const updated = await UserModel.findOneAndUpdate({_id: UID}, data, {new: true})
-    const updated = null
+    const UID = data._id
+    const updated = await UserModel.findOneAndUpdate({_id: UID}, updateData, {new: true})
 
     res.send({
       message: "data akun berhasil diedit",
       akun: updated
     })
   } catch (error) {
-    res.status(500).send({error: error})
+    res.status(500).send({error: error.message})
   }
 }
 
--- routers/akun.js
+++ routers/akun.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const AkunController = require("../controllers/akun")
+const { verifyToken, allowedUser } = require("../helpers")
 
 const AkunRoute = express.Router()
 
-AkunRoute.get("/", AkunController.getAkun)
-AkunRoute.patch("/", AkunController.editAkun)
+AkunRoute.get("/", [verifyToken, allowedUser], AkunController.getAkun)
+AkunRoute.patch("/", [verifyToken, allowedUser], AkunController.editAkun)
 
 module.exports = AkunRoute
