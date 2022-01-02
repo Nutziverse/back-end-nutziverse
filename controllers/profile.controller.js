4b667cd55ab13b86ca9bb9b34394f8ca870eb5d3
lisapriliani <lisaapriliani4@gmail.com>
2022-01-02 21:36:09 +0700

    check

--- controllers/profile.controller.js
+++ controllers/profile.controller.js
@@ -1,16 +1,15 @@
 const { dataToken } = require("../helpers");
-const ProfileModel = require("../models/profile.model");
+const UserModel = require("../models/users.model");
 
-class profileController {
-  static async getProfile(req, res) {
-    const { data } = dataToken(req, res);
-    try {
-      const UID = data._id;
-      const profile = await ProfileModel.findOne({ _id: UID }, { _id, nama, email, no_hp, umur, jeniskelamin, berat, tinggi });
-      res.send(profile);
-    } catch (error) {
-      res.status(500);
-      res.send(error);
-    }
+const getProfile = async (req, res) => {
+  const { data } = dataToken(req, res);
+  try {
+    const UID = data._id;
+    const profile = await UserModel.findOne({ _id: UID }, { _id: 0, nama: 1, email: 1, no_hp: 1, umur: 1, jeniskelamin: 1, berat: 1, tinggi: 1 });
+    res.send(profile);
+  } catch (error) {
+    res.status(500);
+    res.send(error);
   }
-}
+};
+module.exports = getProfile;
--- routers/profile.router.js
+++ routers/profile.router.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const profileController = require("../controllers/profile.controller");
+const { verifyToken, allowedUser } = require("../helpers");
 const router = express.Router();
 
-router.get("/profile", profileController.profileController);
+router.get("/profile", [verifyToken, allowedUser], profileController.getProfile);
 
 const ProfileModel = mongoose.model("Profile", profileSchema);
 module.exports = ProfileSchema;
