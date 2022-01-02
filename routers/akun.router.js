99e54b0e7d1eb7c17dd948a9634c0671bb33d183
Jessica Wong <wongjessica124@gmail.com>
2022-01-02 15:44:47 +0700

    rename akun controller and router

--- routers/akun.router.js
+++ routers/akun.router.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const AkunController = require("../controllers/akun")
+const AkunController = require("../controllers/akun.controller")
 const { verifyToken, allowedUser } = require("../helpers")
 
 const AkunRoute = express.Router()
--- routers/index.js
+++ routers/index.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const AkunRoute = require("./akun")
+const AkunRoute = require("./akun.router")
 
 const router = express.Router()
 
