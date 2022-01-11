3d81d4e2f4af71e4787fedbfab7e762c9ac2498d
Jessica Wong <wongjessica124@gmail.com>
2022-01-11 18:17:50 +0700

    update sign up google

--- routers/users.router.js
+++ routers/users.router.js
@@ -9,5 +9,6 @@ const { verifyToken, verifyTokenWithId } = require("../helpers");
 router.post("/register", usersController.registerUser);
 router.post("/login", usersController.loginUser);
 router.post("/auth/google", usersController.loginByGoogle);
+router.patch("/register/google", usersController.registerByGoogle)
 
 module.exports = router;
